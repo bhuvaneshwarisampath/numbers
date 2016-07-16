6351af3214e3db0db5b7094e5f55917186148e94
bhuvaneshwarisampath <bhuvanasampath26@gmail.com>
2016-07-16 15:25:44 +0530

    Add files via upload

---
+++ Prinfi.java
@@ -0,0 +1,30 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package prinfi;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Student
+ */
+public class Prinfi {
+
+   
+    public static void main(String[] args) {
+        String s;
+        Scanner sc=new Scanner(System.in);
+        System.out.println("enter a character");
+       s=sc.nextLine();
+       for(int i=0;i<5;i++)
+       {
+           System.out.println(s);
+       }
+               
+        
+        
+        
+               
+}}
