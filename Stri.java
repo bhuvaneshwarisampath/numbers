0da2f7acab553b166c8f9d201171135e1efff9a2
bhuvaneshwarisampath <bhuvanasampath26@gmail.com>
2016-07-18 18:08:29 +0530

    Add files via upload

---
+++ Stri.java
@@ -0,0 +1,27 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package stri;
+
+/**
+ *
+ * @author Student
+ */
+public class Stri {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+      String s="revathy";
+      String reverse = new StringBuffer(s).reverse().toString();
+      System.out.println("\n String before reverse:"+s);
+      System.out.println("String after reverse:"+reverse);
+      
+   }
+}
+    
+
